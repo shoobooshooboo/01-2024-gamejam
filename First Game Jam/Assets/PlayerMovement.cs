fc869c8429bb407688957f01eb2eb6f448ed30e6
shoobooshooboo <johnonymous4@gmail.com>
2024-01-13 09:42:36 -0800

    player movement and map with collision

--- First Game Jam/Assets/PlayerMovement.cs
+++ First Game Jam/Assets/PlayerMovement.cs
@@ -5,26 +5,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    PlayerInput playerInput;
-    InputAction moveAction;
+    public PlayerInput playerInput;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
 
-    [SerializeField] Rigidbody rb;
-
-    //private void Start()
-    //{
-    //    playerInput = GetComponent<PlayerInput>();
-    //    moveAction = playerInput.actions.FindAction("Move");
-    //}
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        rb.velocity = new Vector2(1f, 1f);
-        //MovePlayer();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void MovePlayer()
+    private void OnMove(InputValue inputValue)
     {
-        Debug.Log(moveAction.ReadValue<Vector2>());
+        movement = inputValue.Get<Vector2>();
+
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+    }    
 }
